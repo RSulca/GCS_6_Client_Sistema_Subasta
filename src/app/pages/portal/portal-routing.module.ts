e4e2ab39cb6ab518fbc5d9218cbdb3c6a10e0d6f
LuisP <pachasluis730@gmail.com>
2020-11-10 12:42:06 -0500

    correccion import

--- src/app/pages/portal/portal-routing.module.ts
+++ src/app/pages/portal/portal-routing.module.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { AccountComponent } from './account/account.component';
 import { CatalogoComponent } from './catalogo/catalogo.component';
-import { ClienteCompletarComponent } from './cliente-completar/cliente-completar.component';
 import { HomePortalComponent } from './home-portal/home-portal.component';
 import { ProductRegisterComponent } from './product-register/product-register.component';
 import { TermsComponent } from './terms-components/terms-components.component';
