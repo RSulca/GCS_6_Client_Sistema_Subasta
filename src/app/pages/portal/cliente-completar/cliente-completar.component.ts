133f11a2b6ccf25c3e49f26e48846eeb21a992df
JPFR1240 <jean.figueroa124@gmail.com>
2020-11-09 23:40:53 -0500

    dropzone fixed

---
+++ src/app/pages/portal/cliente-completar/cliente-completar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-completar',
+  templateUrl: './cliente-completar.component.html',
+  styleUrls: ['./cliente-completar.component.css']
+})
+export class ClienteCompletarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
