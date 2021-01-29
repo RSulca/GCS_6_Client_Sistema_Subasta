24e3e9aefd9d0f36935ecdb23b46b0e8ff3d7521
JPFR1240 <jean.figueroa124@gmail.com>
2021-01-29 05:17:04 -0500

    ticket 11 done

--- src/app/pages/portal/account/payment-tab/payment-tab.component.ts
+++ src/app/pages/portal/account/payment-tab/payment-tab.component.ts
@@ -1,46 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { LoginService } from 'src/app/services/login.service';
-import { UserEmiterService } from 'src/app/services/user-emiter.service';
-import { UserService } from 'src/app/services/user.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { LoginService } from "src/app/services/login.service";
+import { UserEmiterService } from "src/app/services/user-emiter.service";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
-  selector: 'app-payment-tab',
-  templateUrl: './payment-tab.component.html',
-  styleUrls: ['./payment-tab.component.css']
+  selector: "app-payment-tab",
+  templateUrl: "./payment-tab.component.html",
+  styleUrls: ["./payment-tab.component.css"],
 })
 export class PaymentTabComponent implements OnInit {
-
   user: any = {};
   cardForm: FormGroup;
 
-  constructor(private userInfoEmitter: UserEmiterService, private userService: UserService,
-    private loginService: LoginService, private fb: FormBuilder) {
+  constructor(
+    private userInfoEmitter: UserEmiterService,
+    private userService: UserService,
+    private loginService: LoginService,
+    private fb: FormBuilder
+  ) {
     this.initForm();
   }
 
   ngOnInit(): void {
     this.getUser(this.loginService.usuario._id);
-
   }
 
   getUser(id) {
     this.userService.getUserById(id).subscribe((data: any) => {
       this.user = data.user;
       this.cardForm.patchValue({
-        card: this.user.card,//probar
-      })
-    })
+        card: this.user.card, //probar
+      });
+    });
   }
-
+  validarTarjeta(evento: any) {
+    /* evento.value = evento.value.replace(/[^0-9]/g, ""); */
+    if (evento.charCode >= 48 && evento.charCode <= 57) {
+      return true;
+    }
+    return false;
+  }
   initForm() {
     this.cardForm = this.fb.group({
-      card: ['', []],
+      card: ["", []],
     });
-    this.cardForm.valueChanges.subscribe(data => {
+    this.cardForm.valueChanges.subscribe((data) => {
       this.userInfoEmitter.addCardInformation(data);
-    })
+    });
   }
-
-
 }
